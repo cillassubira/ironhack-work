05f495d3d166ace1b6f2e2fff5a9ccc05e3781c6
Cillas <cillassubira@gmail.com>
2016-04-27 23:27:43 +0200

    Add Week 4 word_central exercise

---
+++ Week4/word_central/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  #root sit#home
+  get "/", to: "site#home"
+  get "/text_inspections/new", to: "text_inspections#new"
+  post "/text_inspections", to: "text_inspections#create"
+  get "/asciis/new", to: "asciis#new"
+  post "/asciis", to: "asciis#create"
+end
